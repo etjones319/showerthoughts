328c1b2b6ef3dfcf09920fa299153f7b3492d613
etjones319 <etjones319@gmail.com>
2019-02-19 08:09:28 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '',
+      link: '',
+      author: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://www.reddit.com/r/showerthoughts.json')
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        let parsedJsonObject = this.parseJsonObject(data);
+        this.setState({
+          title: parsedJsonObject.title,
+          link: parsedJsonObject.link,
+          author: parsedJsonObject.author
+        });
+      });
+  }
+
+  parseJsonObject(data) {
+    let dataChildrenArray = data.data.children;
+    let randomElement = dataChildrenArray[Math.floor(Math.random()*dataChildrenArray.length)];
+    let randomElementData = randomElement.data;
+    let parsedRandomElementData = {
+      title: randomElementData.title,
+      link: 'https://reddit.com' + randomElementData.permalink,
+      author: randomElementData.author
+    }
+
+    return parsedRandomElementData;
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          Shower Thoughts
         </header>
+        <div className="container">
+          <div className="st-detail">
+            <a href={this.state.link}>{this.state.title}</a>
+          </div>
+          <div className="st-author">
+            - {this.state.author}
+          </div>
+        </div>
       </div>
     );
   }
